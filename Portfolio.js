d4c9c3fa0e78ec65827dbb972d31dd823dd53dcb
Charlie.Webs <151995589+DaffyDog@users.noreply.github.com>
2026-06-12 20:53:48 +1000

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,11 +1,15 @@
 let dupdateListEl = document.getElementById("Updatelist")
 let supdateListEl = document.getElementById("SoundUpdatelist")
+let tupdateListEl = document.getElementById("TamagotchiUpdatelist")
 let door = 0
+let sound = 0
+let dragon = 0
 
 dupdateListEl.style.display = "none"
 supdateListEl.style.display = "none"
+tupdateListEl.style.display = "none"
 
-function DoorClickerUpdateList() {
+function doorClickerUpdateList() {
     if (door===0) {
         dupdateListEl.style.display = "block"
         door = 1
@@ -15,13 +19,23 @@ function DoorClickerUpdateList() {
         door = 0
     }
 }
-function SoundboardUpdateList() {
+function soundboardUpdateList() {
     if (door===0) {
         supdateListEl.style.display = "block"
-        door = 1
+        sound = 1
     }
     else {
         supdateListEl.style.display = "none"
-        door = 0
+        sound = 0
+    }
+}
+function dragonUpdateList() {
+    if (dragon===0) {
+        tupdateListEl.style.display = "block"
+        dragon = 1
+    }
+    else {
+        tupdateListEl.style.display = "none"
+        dragon = 0
     }
 }
